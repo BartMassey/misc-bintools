b3ddf2c45b32249a905c3d15df6a6fe5c6c529a8
Bart Massey <bart.massey@gmail.com>
2025-06-27 20:55:12 -0700

    feat: added shuffle

---
+++ README.md
@@ -0,0 +1,16 @@
+# misc-bintools: tiny tools
+Bart Massey 2025
+
+These are Rust rewrites of tiny tools that sit in my
+personal bin and that I use regularly. The C source code for
+some of them is semi-lost, and I'd prefer a solid Rust
+implementation anyhow.
+
+* `shuffle`: Shuffle lines of a file. Can be used as a
+  filter.
+
+## License
+
+This work is made available under the "Apache 2.0 or MIT
+License". See the file `LICENSE.txt` in this distribution for
+license terms.
---
+++ src/bin/shuffle.rs
@@ -0,0 +1,69 @@
+use std::fs::File;
+use std::io::{stdin, stdout, BufReader, BufWriter, Error, Read, Write};
+use std::path::PathBuf;
+use std::process::exit;
+
+use clap::Parser;
+extern crate fastrand;
+
+#[derive(Parser)]
+/// Shuffle a file. With no argument, acts as a shuffle filter.
+struct Args {
+    #[arg(help = "file to shuffle")]
+    filename: Option<PathBuf>,
+}
+
+fn read_file<F: Read>(input: F) -> Result<String, Error> {
+    // XXX Should work on non-UTF8, but won't right now. Ah well.
+    let mut input = BufReader::new(input);
+
+    let mut text = String::new();
+    input.read_to_string(&mut text)?;
+    Ok(text)
+}
+
+fn write_shuffle<F: Write>(output: F, text: String) -> Result<(), Error> {
+    let mut output = BufWriter::new(output);
+
+    let mut lines: Vec<&str> = text.lines().collect();
+    fastrand::shuffle(&mut lines);
+    for l in lines {
+        writeln!(output, "{}", l)?;
+    }
+    Ok(())
+}
+
+#[test]
+fn test_write_shuffle() {
+    use std::io::Cursor;
+
+    for _ in 0..10 {
+        let text = "a\nb\nc\n";
+        let mut dest = Cursor::new(Vec::new());
+        write_shuffle(&mut dest, text.to_string()).unwrap();
+        let mut dest = dest.into_inner();
+        dest.sort();
+        assert_eq!(&dest, b"\n\n\nabc");
+    }
+}
+
+fn run() -> Result<(), Error> {
+    let args = Args::parse();
+    if let Some(filename) = args.filename {
+        let f = File::open(&filename)?;
+        let text = read_file(f)?;
+        let f = File::create(&filename)?;
+        write_shuffle(f, text)?;
+    } else {
+        let text = read_file(stdin().lock())?;
+        write_shuffle(stdout().lock(), text)?;
+    }
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("shuffle: {}", e);
+        exit(1);
+    }
+}
